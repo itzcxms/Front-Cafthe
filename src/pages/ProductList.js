7151127f51b5df330e03f875a830761681bf5ce7
cameron.bonsigne <cameron.bonsigne@gmail.com>
2025-02-10 10:20:56 +0100

    fetch produits

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,7 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 function ProductList(props) {
-    const [produits, setProduits] = useState();
+    const [produits, setProduits] = useState([]);
+
+    useEffect(() => {
+        const fetchProduits = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/api-cafthe");
+                setProduits(response.data);
+            } catch(error) {
+                console.error("Erreur de chargement des produits ", error);
+            }
+        };
+
+        void fetchProduits();
+    }, []);
     return (
         <div>
             <h3>Liste des produits</h3>
